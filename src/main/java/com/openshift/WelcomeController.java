ec3a79fdda6e9d150fb7963539f45c190de763f2
dwaiba <dbanerj@gmail.com>
2017-11-19 17:18:03 +0000

    change Controller

--- src/main/java/com/openshift/WelcomeController.java
+++ src/main/java/com/openshift/WelcomeController.java
@@ -11,7 +11,7 @@ public class WelcomeController {
 
 	// inject via application.properties
 	@Value("${welcome.message:test}")
-	private String message = "Hello World";
+	private String message = "Hello ING Again";
 
 	@RequestMapping("/")
 	public String welcome(Map<String, Object> model) {
